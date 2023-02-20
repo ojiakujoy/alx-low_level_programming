ff0a8a79a9d9df3fa4d53bb32a867d7f293790b8
ojiakujoy <ojiakuuzoamaka131@gmail.com>
2023-02-20 11:07:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- *main - prints "with proper grammar, but the outcome is 
+ * main - prints"with proper grammar, but the outcome is 
  * a piece of art," followed newline
  * Return: Always 0.
  *
@@ -12,5 +12,5 @@ int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }	  
