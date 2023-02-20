dba9929d242dca4964345721c8bd25abe6324fe9
ojiakujoy <ojiakuuzoamaka131@gmail.com>
2023-02-20 10:16:44 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - ptints "with proper grammar, but the outcome is 
+ * a piece of art," foloowed by a new line
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+         printf("with proper grammar, but the outcome is a piece of art,\n");
+          return (0);
+} 	  
